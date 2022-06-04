623e960fde4993a850ea095580cd8ed9bd61cfe1
shoebHamim <hamim99bd@gmail.com>
2022-06-04 22:13:21 +0600

    route testsite deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './Component/Home/Home';
+import Courses from './Component/Courses/Courses';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Welcome to ProDev</h1>
+      <a href="/home">Home</a><br />
+      <a href="/courses">Courses</a>
+      <Router>
+        <Routes>
+          
+          <Route path='/home' element={<Home></Home>}/>
+          <Route path='/courses' element={<Courses></Courses>}></Route>
+
+        </Routes>
+
+
+      </Router>
+
+
+
+
     </div>
   );
 }
